6293898626ff33bc16bed4c7fe7b27213f5f19a7
sungjinkim1986 <sungjin.kim@medithings.co.kr>
2025-11-04 10:03:34 +0900

    refactor.

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,53 +1,77 @@
-import { calculate_crc, string_to_bytes } from '../public/wasm/pkg';
+import init, { calculate_crc, string_to_bytes } from '../public/wasm/pkg';
 
 const NORDIC_UART_SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const TX_CHARACTERISTIC_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 const RX_CHARACTERISTIC_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 
-async function connectToBLEDevice() {
-    if (navigator.bluetooth) {
-        const button = document.getElementById('scanBtn');
-        if (button) {
-            button.addEventListener('click', async () => {
-                try {
-                    const options = {
-                        filters: [{ services: [NORDIC_UART_SERVICE_UUID] }],
-                        optionalServices: [NORDIC_UART_SERVICE_UUID],
-                    };
-                    const device = await navigator.bluetooth.requestDevice(options);
-                    if (device) {
-                        const server = await device.gatt.connect();
-                        if (server.connected) {
-                            const service = await server.getPrimaryService(NORDIC_UART_SERVICE_UUID);
-                            const txCharacteristic = await service.getCharacteristic(TX_CHARACTERISTIC_UUID);
-                            const rxCharacteristic = await service.getCharacteristic(RX_CHARACTERISTIC_UUID);
-                            txCharacteristic.addEventListener('characteristicvaluechanged', (event: any) => {
-                                const value = new TextDecoder().decode(event.target.value);
-                                console.log('Received:', value);
-                                console.log(event.target.value);
-                            });
-                            await txCharacteristic.startNotifications();
+async function main() {
+    await init();
+    const scanBtn = document.getElementById('scanBtn');
+    if (scanBtn) {
+        scanBtn.addEventListener('click', async () => {
+            await toConnect();
+        });
+    }
+}
 
-                            const activate = string_to_bytes('sta?1');
-                            const activateCrc = calculate_crc(activate)
-                            const activatePacket = new Uint8Array([...activate, ...activateCrc]);
-                            await rxCharacteristic.writeValue(activatePacket);
-                            setTimeout(async () => {
-                                const clearing = string_to_bytes('ssr?');
-                                const clearingCrc = calculate_crc(clearing);
-                                const clearingPacket = new Uint8Array([...clearing, ...clearingCrc]);
-                                await rxCharacteristic.writeValue(clearingPacket);
-                            }, 2000);
-                        }
-                    }
-                } catch (error) {
-                    console.error('Error connecting to BLE device:', error);
+async function getRxCharacteristic(server: any) {
+    const service = await server.getPrimaryService(NORDIC_UART_SERVICE_UUID);
+    const tx = await service.getCharacteristic(TX_CHARACTERISTIC_UUID);
+    const rx = await service.getCharacteristic(RX_CHARACTERISTIC_UUID);
+    tx.addEventListener('characteristicvaluechanged', (event: any) => {
+        const value = new TextDecoder().decode(event.target.value);
+        console.log('Received:', value);
+        console.log(event.target.value);
+    });
+    await tx.startNotifications();
+    return rx;
+}
+
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sendMessage(rx: any, input: string) {
+    const bytes = string_to_bytes(input);
+    const crc = calculate_crc(bytes)
+    const packet = new Uint8Array([...bytes, ...crc]);
+    await rx.writeValue(packet);
+}
+
+async function toConnect() {
+    if (navigator.bluetooth) {
+        try {
+            const options = {
+                filters: [{ services: [NORDIC_UART_SERVICE_UUID] }],
+                optionalServices: [NORDIC_UART_SERVICE_UUID],
+            };
+            const device = await navigator.bluetooth.requestDevice(options);
+            if (device) {
+                const server = await device.gatt.connect();
+                if (server.connected) {
+                    const rxCharacteristic = await getRxCharacteristic(server);
+                    // await sendMessage(rxCharacteristic, '');
+                    // await delay(500);
+                    // await sendMessage(rxCharacteristic, '');
+                    // await delay(500);
+                    // await sendMessage(rxCharacteristic, '');
+                    // await delay(500);
+                    // for (let i = 0; i < 24; i++) {
+                    //     await sendMessage(rxCharacteristic, ``);
+                    //     await delay(1000);
+                    // }
+                    //
+                    // await sendMessage(rxCharacteristic, '');
+                    // await delay(3000);
+                    // await sendMessage(rxCharacteristic, '');
                 }
-            });
+            }
+        } catch (error) {
+            console.error('Error connecting to BLE device:', error);
         }
     } else {
         console.log("Web Bluetooth is not supported in this browser.");
     }
 }
 
-connectToBLEDevice();
+await main();
